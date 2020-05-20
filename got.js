5fc3099d7a43fda80cfc21c3397aefd7a166d66f
Nicolas Goris <nicolas.goris@flexso.com>
2020-05-20 08:29:17 +0200

    Add call

--- got.js
+++ got.js
@@ -1,6 +1,14 @@
-var sUrl = "";
+var sUrl = "https://api.got.show/api/book/characters/";
 
-function getData(sChar) {}
+function getData(sChar) {
+  $.getJSON(sUrl + sChar)
+    .done(function (oData) {
+      console.log(oData);
+    })
+    .fail(function (oError) {
+      console.log(oError);
+    });
+}
 
 function getCharacter() {
   var sChar = $("#charactername").val();
